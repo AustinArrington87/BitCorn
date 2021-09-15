562336a055e078d3857ebf2879e7b4ef0511d0eb
Austin Arrington <austinbarrington@gmail.com>
2021-09-15 15:05:46 -0400

    Update mint-nft.js

--- scripts/mint-nft.js
+++ scripts/mint-nft.js
@@ -1,7 +1,7 @@
 require("dotenv").config()
 
 console.log(process.env.API_URL)
-
+//const API_URL = process.env.API_URL;
 const API_URL = "https://eth-ropsten.alchemyapi.io/v2/T0QVTK9-9Qky6mCT-7Nf7FdTrAb3OWzi"
 const { createAlchemyWeb3 } = require("@alch/alchemy-web3")
 const web3 = createAlchemyWeb3(API_URL)
