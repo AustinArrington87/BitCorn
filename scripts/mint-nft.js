7f5af4c939f41053c07dfb8f1f77422fc0b239b1
Austin Arrington <austinbarrington@gmail.com>
2021-09-15 14:30:56 -0400

    Create mint-nft.js

---
+++ scripts/mint-nft.js
@@ -0,0 +1,11 @@
+require("dotenv").config()
+
+console.log(process.env.API_URL)
+
+const API_URL = "https://eth-ropsten.alchemyapi.io/v2/T0QVTK9-9Qky6mCT-7Nf7FdTrAb3OWzi"
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3")
+const web3 = createAlchemyWeb3(API_URL)
+
+// grab contract ABI (Application Binary Interface)
+const contract = require("../artifacts/contracts/MyNFT.sol/MyNFT.json")
+console.log(JSON.stringify(contract.abi))
