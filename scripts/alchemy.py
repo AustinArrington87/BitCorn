80935198fc258e03c6d99f9c05b2c6a308c65bf1
Austin Arrington <austinbarrington@gmail.com>
2021-09-15 01:13:40 -0400

    Create alchemy.py

---
+++ scripts/alchemy.py
@@ -0,0 +1,21 @@
+import requests
+import json
+# test get balance with Alchemy composer key https://composer.alchemyapi.io/
+# https://docs.alchemy.com/alchemy/introduction/why-use-alchemy
+# enter API from app from Alchemy UI
+url = 'https://eth-ropsten.alchemyapi.io/v2/T0QVTK9-9Qky6mCT-7Nf7FdTrAb3OWzi'
+headers = {'content-type': 'application/json'}
+# enter Address of Metamask Wallet you are getting balance for (test on Ropsten)
+balance = {
+    "jsonrpc":"2.0",
+    "method":"eth_getBalance",
+    "params":["0xc94770007dda54cF92009BFF0dE90c06F603a09f", "latest"],
+    "id":0
+}
+# get Balance (integer of current balance in Wei)
+def getBalance(balance):
+    balanceInteger = requests.post(url, data = json.dumps(balance), headers=headers)
+    return balanceInteger.text
+
+print(getBalance(balance))
+# Result is in wei, not ETH. Wei is used as the smallest denomination of ether. The conversion from wei to ETH is 1 eth = 1018 wei. So if we convert 0xde0b6b3a7640000 to decimal we get 1*1018 wei, which equals 1 ETH
