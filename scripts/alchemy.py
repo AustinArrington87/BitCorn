9c684bca9c20c24ded86c1c4ee559d035cfd4bea
Austin Arrington <austinbarrington@gmail.com>
2021-09-15 10:43:02 -0400

    Update alchemy.py
    convert balance from string to Wei int to ETH

--- scripts/alchemy.py
+++ scripts/alchemy.py
@@ -1,21 +1,42 @@
 import requests
 import json
+import sys
+from decimal import Decimal
+from web3 import Web3
+#https://web3py.readthedocs.io/en/stable/web3.main.html
+
 # test get balance with Alchemy composer key https://composer.alchemyapi.io/
 # https://docs.alchemy.com/alchemy/introduction/why-use-alchemy
 # enter API from app from Alchemy UI
-url = 'https://eth-ropsten.alchemyapi.io/v2/<enterAPIKey>'
+url = 'https://eth-ropsten.alchemyapi.io/v2/T0QVTK9-9Qky6mCT-7Nf7FdTrAb3OWzi'
 headers = {'content-type': 'application/json'}
-# enter Address of Metamask Wallet you are getting balance for (test on Ropsten)
+# enter API from app from Alchemy UI - this is for Ropsten test network
+mmWalletAddress = "0x33dE1309578F0b8F478F94A323a80abc5903255F"
+# wallet address from contracts/MyNFT.sol 0xDCfa0E058796cEBA7bbe554eeEf787f66e25ca0A
+# wallet address from token/CORN1.0.sol
+
 balance = {
     "jsonrpc":"2.0",
     "method":"eth_getBalance",
-    "params":["<enter Metamask key>", "latest"],
+    "params":[mmWalletAddress, "latest"],
     "id":0
 }
-# get Balance (integer of current balance in Wei)
+# get Balance
 def getBalance(balance):
     balanceInteger = requests.post(url, data = json.dumps(balance), headers=headers)
-    return balanceInteger.text
+    #integer of the current balance for the given address in wei.
+    wei = balanceInteger.text
+    return wei
 
-print(getBalance(balance))
-# Result is in wei, not ETH. Wei is used as the smallest denomination of ether. The conversion from wei to ETH is 1 eth = 1018 wei. So if we convert 0xde0b6b3a7640000 to decimal we get 1*1018 wei, which equals 1 ETH
+def convertBalance2Int (weiBalance):
+    wei = Web3.toInt(hexstr=weiBalance)
+    eth = Web3.fromWei(Web3.toInt(hexstr=weiBalance),'ether')
+    return {"wei": wei, 'eth': eth}
+
+# Result is in wei, not ETH. Wei is used as the smallest denomination of ether. The conversion from wei to ETH is 1 eth = 10^18 wei. So if we convert 0xde0b6b3a7640000 to decimal we get 1*1018 wei, which equals 1 ETH
+weiBalance = json.loads(getBalance(balance))["result"]
+print(weiBalance, "balance for MetaMask wallet address ", mmWalletAddress)
+
+balanceInt = convertBalance2Int(weiBalance)
+print("Balance (Wei): ", balanceInt["wei"])
+print("Balance (ETH): ", balanceInt["eth"])
