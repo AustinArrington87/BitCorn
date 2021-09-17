28a573d1915a6067ea2b463bf621ece2f6ab2dc0
Austin Arrington <austinbarrington@gmail.com>
2021-09-17 00:47:11 -0400

    Update mint-nft.js
    mint-NFT v1 functional

--- scripts/mint-nft.js
+++ scripts/mint-nft.js
@@ -1,11 +1,53 @@
 require("dotenv").config()
+const API_URL = "https://eth-ropsten.alchemyapi.io/v2/T0QVTK9-9Qky6mCT-7Nf7FdTrAb3OWzi";
+const PUBLIC_KEY = process.env.PUBLIC_KEY
+const PRIVATE_KEY = process.env.PRIVATE_KEY
 
-console.log(process.env.API_URL)
-//const API_URL = process.env.API_URL;
-const API_URL = "https://eth-ropsten.alchemyapi.io/v2/T0QVTK9-9Qky6mCT-7Nf7FdTrAb3OWzi"
 const { createAlchemyWeb3 } = require("@alch/alchemy-web3")
 const web3 = createAlchemyWeb3(API_URL)
 
-// grab contract ABI (Application Binary Interface)
 const contract = require("../artifacts/contracts/MyNFT.sol/MyNFT.json")
-console.log(JSON.stringify(contract.abi))
+const contractAddress = "0xDCfa0E058796cEBA7bbe554eeEf787f66e25ca0A"
+const nftContract = new web3.eth.Contract(contract.abi, contractAddress)
+
+async function mintNFT(tokenURI) {
+  const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, "latest") //get latest nonce
+
+  //the transaction
+  const tx = {
+    from: PUBLIC_KEY,
+    to: contractAddress,
+    nonce: nonce,
+    gas: 500000,
+    data: nftContract.methods.mintNFT(PUBLIC_KEY, tokenURI).encodeABI(),
+  }
+
+  const signPromise = web3.eth.accounts.signTransaction(tx, PRIVATE_KEY)
+  signPromise
+    .then((signedTx) => {
+      web3.eth.sendSignedTransaction(
+        signedTx.rawTransaction,
+        function (err, hash) {
+          if (!err) {
+            console.log(
+              "The hash of your transaction is: ",
+              hash,
+              "\nCheck Alchemy's Mempool to view the status of your transaction!"
+            )
+          } else {
+            console.log(
+              "Something went wrong when submitting your transaction:",
+              err
+            )
+          }
+        }
+      )
+    })
+    .catch((err) => {
+      console.log("Promise failed:", err)
+    })
+}
+
+mintNFT(
+  "https://gateway.pinata.cloud/ipfs/Qmdd1fJ7MUiWSGG7ocUekJc9zR1na2e17xi7VWaTDc7Wp9"
+)
